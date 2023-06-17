d6ddfc36becb8f73eb762dd2571d34969424948b
Paarkavi Priya S <paarkavi19092001@gmail.com>
2023-06-17 13:47:09 +0530

    .ADD expensesRecucer.js added for state and action

--- expenses.frontend/src/services/expenses.js
+++ expenses.frontend/src/services/expenses.js
@@ -6,8 +6,7 @@ export const GetExpenses = async (dispatch) => {
             {id: 2, description :'Gas', amount: 200},
             {id: 3, description :'Student loans', amount: 10000},
             {id: 4, description :'shopping', amount: 3000},
-
-        ]
+        ];
 
     } catch{
         console.log('Error!')
