94edb40e4008d198c89cc9302df489c2c350ff44
Paarkavi Priya S <paarkavi19092001@gmail.com>
2023-06-17 13:40:24 +0530

    .ADD added expenses.js for api call

---
+++ expenses.frontend/src/services/expenses.js
@@ -0,0 +1,15 @@
+export const GetExpenses = async (dispatch) => {
+    try{
+        //api call
+        const expenses = [
+            {id: 1, description :'Groceries', amount: 100},
+            {id: 2, description :'Gas', amount: 200},
+            {id: 3, description :'Student loans', amount: 10000},
+            {id: 4, description :'shopping', amount: 3000},
+
+        ]
+
+    } catch{
+        console.log('Error!')
+    }
+}
