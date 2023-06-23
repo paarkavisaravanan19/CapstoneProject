3837f976c11213c9fe6783881f6521c1daefb692
Paarkavi Priya S <paarkavi19092001@gmail.com>
2023-06-23 11:07:42 +0530

    .UPDATE dependency injection added

--- Expenses.WebApi/Expenses.Core/ExpensesServices.cs
+++ Expenses.WebApi/Expenses.Core/ExpensesServices.cs
@@ -1,7 +1,20 @@
-﻿namespace Expenses.Core
-{
-    public class Class1
-    {
+﻿using Expenses.DB;
 
+namespace Expenses.Core
+{
+    public class ExpensesServices : IExpensesServices
+    {
+        //creating constructor
+        private AppDbContext _context;
+        public ExpensesServices(AppDbContext context)
+        {
+            //dependemcy injection used to show instance of dbcontext
+            _context = context;
+        }
+
+        public List<Expense> GetExpenses()
+        {
+            return _context.Expenses.ToList();
+        }
     }
 }
