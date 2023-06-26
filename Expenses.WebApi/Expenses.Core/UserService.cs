22c291f0380955866d09af60cbe8b59190b8fd77
Paarkavi Priya S <paarkavi19092001@gmail.com>
2023-06-27 00:55:23 +0530

    .ADD Authentication services added

---
+++ Expenses.WebApi/Expenses.Core/UserService.cs
@@ -0,0 +1,19 @@
+﻿using Expenses.Core.DTO;
+using Expenses.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expenses.Core
+{
+    public class UserService : IUserService
+    {
+        //dependency injection
+        public Task<AuthenticatedUser> SignUp(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
