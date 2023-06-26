a6abf9d64fc328b7f950e82c4d31e099168fa5b4
Paarkavi Priya S <paarkavi19092001@gmail.com>
2023-06-26 14:18:34 +0530

    .ADD react-toastify has been added

--- expenses.frontend/src/App.js
+++ expenses.frontend/src/App.js
@@ -1,8 +1,12 @@
+import { ToastContainer } from "react-bootstrap";
 import ExpenseForm from "./components/ExpenseForm";
 import ExpenseList from "./components/ExpenseList";
+import { ToastContainer } from "react-toastify";
 
+//visualizing the toast notification
 const App =() =>(
 <div style={{width: '60%', margin : 'auto'}}>
+  <ToastContainer/>
   <h3>New Expense</h3>
   <ExpenseForm/>
   <hr style={{border:'1px solid grey'}}/>
