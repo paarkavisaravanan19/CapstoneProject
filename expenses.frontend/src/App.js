fc57b1019d60dcd33bf02806aeaa1e2e58e5c522
Paarkavi Priya S <paarkavi19092001@gmail.com>
2023-06-28 00:53:18 +0530

    .UPDATE routing done

--- expenses.frontend/src/App.js
+++ expenses.frontend/src/App.js
@@ -3,11 +3,24 @@ import ExpenseList from "./components/ExpenseList";
 import { ToastContainer } from "react-toastify";
 import SignInPage from "./components/SignInPage";
 import SignUpPage from "./components/SignUpPage";
+import { BrowserRouter } from "react-router-dom";
+import { useSelector } from "react-redux";
+import HomePage from "./components/HomePage";
 
 
 //visualizing the toast notification
-const App =() =>(
-  <SignUpPage />
-);
+const App =() =>
+{
+  const {isLoggedIn} = useSelector(state => state.authenticationSlice);
+  //routing the pages based on condition
+  return 
+  <BrowserRouter>
+  <Switch>
+    <Route exact path = '/' render={()=> (isLoggedIn? <HomePage/> : <SignInPage/>)}/>
+    <Route path = '/signup' render={()=>(isLoggedIn? <Redirect to='/' /> : <SignUpPage/>)}/>
+    <Route path = '/signin' render={()=>(isLoggedIn? <Redirect to='/' /> : <SignInPage/>)}/>
+  </Switch>
+  </BrowserRouter>
+}
 
 export default App;
