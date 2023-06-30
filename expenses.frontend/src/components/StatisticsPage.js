f97d4fda42a5d61921febca91e249d92524502b6
Paarkavi Priya S <paarkavi19092001@gmail.com>
2023-06-30 12:06:04 +0530

    .UPDATE testing Doughnut with dummy data

--- expenses.frontend/src/components/StatisticsPage.js
+++ expenses.frontend/src/components/StatisticsPage.js
@@ -1,5 +1,31 @@
 import { Doughnut } from "react-chartjs-2"; 
 
-const StatisticsPage = () =>{
-    
-}
+const StatisticsPage = () =>
+{
+    const data={
+        lables : ['Shopping', 'Credit Card'],
+        datasets : [{
+            data:[10, 3],
+            backgroundColor : [
+                '#007bff', //blue
+                '#FF0000', //red
+                '#FFD700', //yellow
+                '#28a745', //green
+                '#FF00FF', //violet
+                '#ff9900', //orange
+                '#00FFFF', //aqua marine
+                '#d69ae5', //red violet
+                '#FF8F66', //orange red
+                '#00FF00', //lime
+            ]
+        }]
+    }
+    return <div
+    style={{maxWidth:'35rem',maxHeight:'35rem', margin:'auto', textAlign:'center'}}>
+        <h4 style={{marginTop:'10px'}}>Expenses per category</h4>
+        <Doughnut data={data}/>
+
+    </div>
+};
+
+export default StatisticsPage;
